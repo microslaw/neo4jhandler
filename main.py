56644a6b675ae738e5fda95a5a758f3d8c626f4b
microslaw <milosz.grunwald@gmail.com>
2024-10-10 22:44:54 +0200

    add basic display

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from neo4j import GraphDatabase
-
+import os
+from pyvis.network import Network
 import hidden
+import networkx as nx
+import webbrowser
+from neo4j.graph import Node, Relationship
 
 # e.g. hidden.py:
 #
@@ -8,8 +12,47 @@ import hidden
 # user="neo4j"
 # password="password"
 
+class NodeType:
+    def __init__(self, node):
+        self.id = int(node.element_id.split(":")[-1])
+        self.properties = {}
+        self.labels = set(node.labels)
 
-def check_connection():
+        for key in node.keys():
+            self.properties[key] = node.get(key)
+
+    def to_JSON(self):
+        return {
+            "id": self.id,
+            "properties": self.properties,
+            "labels": list(self.labels)
+        }
+
+
+class RelationType:
+    def __init__(self, record):
+        left, right = record[0].nodes
+        self.nodes =NodeType(left), NodeType(right)
+
+        # !!! might not be unique, look into later
+        self.id = record[0].element_id
+        self.type = record[0].type
+        self.properties = {}
+
+        for key in record[0].keys():
+            self.properties[key] = record[0].get(key)
+
+    def to_JSON(self):
+        return {
+            "id": self.id,
+            "nodes": [node.to_JSON() for node in self.nodes],
+            "type": self.type,
+            "properties": self.properties
+        }
+
+
+
+def check_connection() -> None:
     try:
         with GraphDatabase.driver(
             hidden.uri, auth=(hidden.username, hidden.password)
@@ -21,18 +64,51 @@ def check_connection():
         exit(1)
 
 
-def execute_query(query, database="neo4j"):
+def execute_query(query:str, database:str="neo4j")-> tuple:
     with GraphDatabase.driver(
-        hidden.uri, auth=(hidden.username, hidden.password)
+        hidden.uri, auth=(hidden.username, hidden.password), database=database
     ) as driver:
-        with driver.session(database=database) as session:
-            result = session.run(query)
-            return result
+        records, summary, keys = driver.execute_query(query)
+
+        return records, summary, keys
+
+
+def relations_to_graph(relations: list[RelationType]) -> nx.MultiDiGraph:
+
+    G = nx.MultiDiGraph()
+
+    for relation in relations:
+        left, right = relation.nodes
+
+        G.add_node(left.id, data=left.to_JSON())
+        G.add_node(right.id, data=right.to_JSON())
+        G.add_edge(left.id, right.id, data=relation.to_JSON())
+
+    return G
+
+
+def display_graph(G:nx.Graph):
+
+    nx.draw(G, with_labels=True)
+    nt = Network(height="1000", width="100%")
+    nt.from_nx(G)
+
+    if not os.path.exists("tmp"):
+        os.makedirs("tmp")
+    nt.save_graph("tmp/nx.html")
+    url = f"file:///{os.getcwd()}/tmp/nx.html"
+    webbrowser.open(url, new=0, autoraise=True)
+
+
 
 
 check_connection()
-with open("queries/test.cypher", "r") as f:
+with open("queries/check.cypher", "r") as f:
     query = f.read()
 
-result = execute_query(query)
-print(result)
+records, summary, keys = execute_query(query)
+relations = [RelationType(relation) for relation in records]
+
+G = relations_to_graph(relations)
+
+display_graph(G)
